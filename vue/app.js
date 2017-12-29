6b09eb1212e60ca653df2f01107b5dabeea55e96
dhirajs1997 <32067769+dhirajs1997@users.noreply.github.com>
2017-12-29 18:42:19 +0530

    Comments added to Vue.events
    Vue.js works on github.
    Added comments to code and changed title.

--- vue/app.js
+++ vue/app.js
@@ -2,6 +2,7 @@ var mainVue = new Vue({
   el:'#vue-app',
   data: {
     age: 20,
+    // will change to coordinates after on hovering over canvas
     x: 'Hover mouse ',
     y: 'to view co-ordinates'
   },
@@ -12,8 +13,11 @@ var mainVue = new Vue({
     sub: function(dec){
       this.age -= dec;
     },
+    //This event will update xy parameters
     updateXY: function(event){
+      //To see parameters on console
       console.log(event);
+      // get x and y coordinates and display as (x,y)
       this.x = '('+event.offsetX+',';
       this.y = event.offsetY+')';
     }
